331b5c9cc309c8f33c72025b76c63ca18f238bdb
matteocng <matteocng@outlook.com>
2018-07-25 02:20:44 +0200

    chore(flow): upgrade flow-bin to version 0.77 and run 'flow-typed update'

--- flow-typed/npm/classnames_v2.x.x.js
+++ flow-typed/npm/classnames_v2.x.x.js
@@ -1,15 +1,23 @@
-// flow-typed signature: 3fba12a77525f37b0492c8dab3e04f0e
-// flow-typed version: 94e9f7e0a4/classnames_v2.x.x/flow_>=v0.28.x
+// flow-typed signature: cf86673cc32d185bdab1d2ea90578d37
+// flow-typed version: 614bf49aa8/classnames_v2.x.x/flow_>=v0.25.x
 
 type $npm$classnames$Classes =
-  string |
-  {[className: string]: ?boolean } |
-  Array<string> |
-  void |
-  null
+  | string
+  | { [className: string]: * }
+  | false
+  | void
+  | null;
 
-declare module 'classnames' {
-  declare function exports(
-    ...classes: Array<$npm$classnames$Classes>
-  ): string;
+declare module "classnames" {
+  declare module.exports: (
+    ...classes: Array<$npm$classnames$Classes | $npm$classnames$Classes[]>
+  ) => string;
+}
+
+declare module "classnames/bind" {
+  declare module.exports: $Exports<"classnames">;
+}
+
+declare module "classnames/dedupe" {
+  declare module.exports: $Exports<"classnames">;
 }
